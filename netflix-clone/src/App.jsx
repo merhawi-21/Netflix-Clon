241d740952ee4c61b782c7b98ff923a60a602373
Merhawi <merhawiainealem@gmail.com>
2024-11-15 00:28:16 +0100

    All features are checked

--- netflix-clone/src/App.jsx
+++ netflix-clone/src/App.jsx
@@ -1,14 +1,40 @@
-import { useState } from 'react'
-
+import { useEffect, useState } from 'react'
 import './App.css'
-
+import Home from './pages/Home/Home'
+import {Routes,Route, useNavigate} from 'react-router-dom'
+import Login from './pages/Login/Login'
+import Player from './pages/Player/Player'
+import { onAuthStateChanged } from 'firebase/auth'
+import { auth } from './firebase'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+  
 function App() {
-  const [count, setCount] = useState(0)
+
+  const navigate = useNavigate();
+
+  useEffect(()=>{
+    onAuthStateChanged(auth, async (user)=>{
+      if(user){
+        console.log("Logged In");
+        navigate('/');
+      }
+      else{
+        console.log("Logged Out")
+        navigate('/login')
+      }
+    })
+  },[])
 
   return (
     <>
-      <h1>Working</h1>
-        
+    <ToastContainer theme='dark'/>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/login' element={<Login/>} />
+        <Route path='/player/:id' element={<Player/>} />
+      </Routes>
+           
     </>
   )
 }
