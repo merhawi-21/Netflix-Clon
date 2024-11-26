e73b20a2c21098037ab40fe5254232bc7dacefe3
Merhawi <merhawiainealem@gmail.com>
2024-11-27 00:31:27 +0100

    title card css modified

--- netflix-clone/src/pages/Home/Home.jsx
+++ netflix-clone/src/pages/Home/Home.jsx
@@ -26,7 +26,7 @@ const Home = () => {
       </div> 
       </div>
       <div className="more-cards">
-      <TitleCards title={"Blockbuster Movies"} category={"top_rated"}/>  
+      <TitleCards title={"Blockbuster Movies"} category={"top_rated"} />  
       <TitleCards title={"Only on Netflix"} category={"popular"}/>  
       <TitleCards title={"Upcoming"} category={"upcoming"}/>  
       <TitleCards title={"Top pics for you"} category={"now_playing"}/>  
